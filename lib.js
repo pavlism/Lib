9fb25c563c487d7363d9809b891773b02b1c11f7
mark pavlis <mark.pavlis@ugdsb.on.ca>
2017-06-29 08:45:30 -0400

    finished

--- lib.js
+++ lib.js
@@ -1,15 +1,4 @@
-//V0.1
-
-//Logging object, this is used to log informaiton at different levels to the console window on the client side and the
-//console window n the server side.
-
-var Logger = require("mrp-logger");
+//Just the Lib object.
 
 Lib = {};
-Lib.test = function(){
-    console.log('sdfsd');
-    var log = new Logger('sdf',Logger.level.trace);
-    log.trace('test');
-}
-
 module.exports = Lib;
